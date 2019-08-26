9fd4384695108cae443cb5ab49f975a6a473580b
generic-callback-1830 <hadeeqasaman@yahoo.com>
2019-08-26 06:27:25 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout (string) {
+  return string.toUpperCase ()
+}
+function whisper (string) {
+  return string.toLowerCase ()
+}
+function logShout (string) {
+  string = "hello".toUpperCase();
+  console.log(string)
+}
+function logWhisper (string) {
+  string = "hello".toLowerCase();
+  console.log (string)
+}
+function sayHiToGrandma (string) {
+  string = "hello"
+  var gma = 'I can\'t hear you!';
+  if (string === string.toLowerCase()){
+    return gma
+  var x = "Yes Indeed!"
+  if (string === string.toUpperCase()){
+    return (x - gma)
+  var prime = "I love you, too.";
+  if (string === "I love you, Grandma."){
+    return prime;
+  }
+  }
+  }
+    }
+  
+  
